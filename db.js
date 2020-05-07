e421869dbbbb41a3fbf63e63d09ce324d269b227
LeeJongbok <s2feeling@naver.com>
2020-05-07 23:13:55 +0900

    Update db.js

--- db.js
+++ db.js
@@ -35,9 +35,14 @@ mongoose.connect(
 // 실제 connection 인스턴스를 만듦
 const db = mongoose.connection;
 
-// handleOpen과 handleError는  
+// handleOpen과 handleError는 DB의 연결과 연결 에러를 나타내기 위한 콜백 함수로서의 기능을 함   
 const handleOpen = () => console.log("Connected to DB");
 const handleError = error => console.log("Error on DB Connection:${error}");
 
+// db가 연결되어서 open되게 되면 handleOpen 함수가 실행된다
+// handleOpen 함수가 실행되면 미리 입력한 console.log 문장이 출력되어서 DB가 잘 연결되었음을 확인할 수 있다. 
 db.once("open", handleOpen);
+
+// db 연결에 에러가 발생하면 handleError 함수가 실행된다
+// handleError 함수가 실행되면 미리 입력한 console.log 문장이 출력되어서 에러 여부를 확인할 수 있다. 
 db.on("error", handleError);
