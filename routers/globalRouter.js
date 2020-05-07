d7e75a20569d1306f078a002dd556667c381dc0f
LeeJongbok <s2feeling@naver.com>
2020-05-07 23:35:01 +0900

    Update globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -3,6 +3,12 @@ import passport from "passport";
 import routes from "../routes";
 import {home, getJoin, postJoin, getLogin, postLogin, logout, intro, myLibrary, myPage, bookPage, page1, facebookLogin, postFacebookLogin } from '../controllers/userControllers';
 
+
+// Router()메소드는 새로운 router 객체를 만든다
+// router 객체는 미들웨어 및 라우트의 분리된 인스턴스임
+// 즉, 미들웨어 및 라우팅 기능만을 수행하는 '미니 앱'이라고 할 수 있음
+// 여기서는 globalRouter = express.Router()를 통해 
+// router 객체인 globalRouter로 라우팅 기능을 수행하도록 함 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
