d1cbbd9032299acf321d947c004427aca06ac12a
LeeJongbok <s2feeling@naver.com>
2020-05-12 15:01:01 +0900

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -20,17 +20,24 @@ export const getJoin = (req, res) => res.render("join");
 // register메소드는 passport-local-mongoose의 메서드임
 
 // 또한 async-await 패턴을 사용함
-// 이 패턴을 사용하는 이유는 User 객체가 register된 후에
-// 다음 로직이 실행되게 하기 위함임
+// 이 패턴을 사용하는 이유는 User 객체가 register된 후에 다음 로직이 실행되게 하기 위함임
 // 자바스크립트는 비동기 방식으로 실행되기 때문에
-// async-await 패턴을 사용하지 않으면, User 객체가 register되지 않고 
-// 함수가 종료될 수 있음
+// async-await 패턴을 사용하지 않으면, User 객체가 register되지 않고 함수가 종료될 수 있음
+// 비동기 방식이란 하나의 로직이 요청을 한 후에, 그 요청의 리턴을 기다리지 않고,
+// 바로 다음 로직이 실행되는 방식의 프로그래밍을 의미함 
 // 따라서 async-await와 같은 비동기 처리 패턴을 사용해야 함 
 export const postJoin = async (req, res) => {
+    
+    // 다음과 같이 req객체의 parameter인 
+    // email과 password를 추출할 수 있음
+    // 이렇게 해주는 이유는 뒤에서 User 객체를 만들 때,
+    // email과 password를 활용해주기 위함임 
     const {
         body: {email, password}
     } = req;
 
+    // try-catch문을 사용함
+    // try-catch문을 사용하는 이유는 
     try{        
         const user = new User({
             email
