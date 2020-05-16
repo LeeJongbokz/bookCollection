df3766417606718c40da6b0e0d2c2e364f882e7f
LeeJongbok <s2feeling@naver.com>
2020-05-16 23:49:03 +0900

    Update globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -12,7 +12,7 @@ import passport from "passport";
 import routes from "../routes";
 
 // userControllers.js에 선언한 컨트롤러 콜백함수들을 import함
-// import하는 이유는 여기서 라우트를 통해 들어오는 요청에 대해 해당 콜백함수를 매칭시켜주기 위함임  
+// 그 이유는 여기서 라우트를 통해 들어오는 요청에 대해 해당 콜백함수를 매칭시켜주기 위함임  
 import {home, getJoin, postJoin, getLogin, postLogin, logout, intro, myLibrary, myPage, bookPage, page1, facebookLogin, postFacebookLogin } from '../controllers/userControllers';
 
 
@@ -23,23 +23,17 @@ import {home, getJoin, postJoin, getLogin, postLogin, logout, intro, myLibrary, 
 // router 객체인 globalRouter로 라우팅 기능을 수행하도록 함 
 const globalRouter = express.Router();
 
-// globalRouter.get(routes.home, home)은 
-// routes.home으로 들어오는 get요청에 대해서
-// 콜백함수인 home을 실행시킴
+// routes.home으로 들어오는 get요청에 대해 콜백함수인 home을 실행시킴
 // 콜백함수인 home은 userControllers.js에서 만들어서 import함
 // 다른 콜백함수들도 마찬가지로 userControllers.js에서 만들어서 import함
 globalRouter.get(routes.home, home);
 
-// globalRouter.get(routes.join, getJoin)은
-// routes.join으로 들어오는 get요청에 대해서
-// 콜백함수인 getJoin을 실행시킴 
+// routes.join으로 들어오는 get요청에 대해 콜백함수인 getJoin을 실행시킴 
 globalRouter.get(routes.join, getJoin);
 
-// globalRouter.post(routes.join, postJoin, postLogin)은
-// routes.join으로 들어오는 post요청에 대해서
-// 콜백함수인 postJoin을 실행시킴
-// 이 때, 콜백함수인 postJoin은 미들웨어 함수로서 기능해
-// 함수 실행이 끝나면, 다음 콜백함수인 postLogin 함수가 실행됨
+// routes.join으로 들어오는 post요청에 대해 콜백함수인 postJoin을 실행시킴
+// 이 때, 콜백함수인 postJoin은 미들웨어 함수임
+// 따라서 함수 실행이 끝나면, 다음 콜백함수인 postLogin 함수가 실행됨
 globalRouter.post(routes.join, postJoin, postLogin);
 
 // 이하 동일
@@ -76,6 +70,5 @@ globalRouter.get(
 )
 
 // globalRouter 객체를 export함
-// 이 객체를 export하는 이유는 app.js에서
-// import하기 위함임 
+// 이 객체를 export하는 이유는 app.js에서 import하기 위함임 
 export default globalRouter;
