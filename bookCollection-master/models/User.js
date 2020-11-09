b9b76a425c76dda4e5109c08b67f53d2e964f2c4
LeeJongbokZ <s2feeling@naver.com>
2020-11-09 20:33:08 +0900

    project

--- bookCollection-master/models/User.js
+++ bookCollection-master/models/User.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
-import crypto from "crypto";
 import passportLocalMongoose from "passport-local-mongoose";
 
 const UserSchema = new mongoose.Schema({
     
     email: {type: String, required: true, unique: true, 'default': ''},
-    hashed_password: {type: String, required: true, 'default': ''},
+    password: {type: String, required: true, 'default': ''},
     salt: {type: String, required: true},
     avatarUrl: {type: String},
     friends:[
@@ -41,26 +40,6 @@ const UserSchema = new mongoose.Schema({
     ]
 
 })
-
-UserSchema.virtual('password').set(function(password){
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashed_password = this.encryptPassword(password);
-}).get(function(){return this._password});
-
-
-UserSchema.method('encryptPassword', function(plainText, inSalt){
-    if(inSalt){
-        return crypto.createHmac('sha1', inSalt).update(plainText).digest('hex');
-    }else{
-        return crypto.createHmac('sha1', this.salt).update(plainText).digest('hex');
-    }
-});
-
-UserSchema.method('makeSalt', function(){
-    return Math.round((new Date().valueOf()*Math.random())) +'';
-});
-
 
 UserSchema.plugin(passportLocalMongoose, { usernameField: "email"});
  
