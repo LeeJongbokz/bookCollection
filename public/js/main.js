3b52e0bca4ebde677f6817104261d3f7703b8160
LeeJongbokZ <s2feeling@naver.com>
2020-11-12 22:23:43 +0900

    project

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,13 @@
-$.ajax({
-    type: "GET",
-    url: "https://graph.facebook.com/v9.0/me?fields=friends&access_token=EAAHWs5TlHU4BAAcOEMBSfIxlxUN76eAuO37vyZAFuXTxESSNE2igcxuu52FvVk0idPLUlGu4z2W2BVL5BN4Dk2cdUhCEGWMXrKYkdZBoU2NxZAGNFlLXtDnlHzVeZAVKhry3QqxJm2Rggft2cVRBbQB1Fh8VhfoOwt3ruTRWMPCAbbaAZBo6Ut7dQDEJ9mh0ZD",
-    data: {},
-    success: function(response){
-      console.log(response)
-    }
-  })
+
+
+
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: "https://graph.facebook.com/v9.0/me?fields=friends&access_token=EAAHWs5TlHU4BAAcOEMBSfIxlxUN76eAuO37vyZAFuXTxESSNE2igcxuu52FvVk0idPLUlGu4z2W2BVL5BN4Dk2cdUhCEGWMXrKYkdZBoU2NxZAGNFlLXtDnlHzVeZAVKhry3QqxJm2Rggft2cVRBbQB1Fh8VhfoOwt3ruTRWMPCAbbaAZBo6Ut7dQDEJ9mh0ZD",
+        data: {},
+        success: function(response){
+            console.log(response)
+        }
+    })
+});
